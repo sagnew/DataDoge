8f015295d18ce1499851166830f4e274c6006ed2
Manuel Lopez <mjl288@scarlemail.rutgers.edu>
2014-06-14 11:07:54 -0400

    basic flask shit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/test')
+def test():
+	return 'test'
+
+if __name__ == '__main__':
+	# if I am being run from the command line.
+	app.run('0.0.0.0', debug = True)
