dbca5c515120f12aa205094b84301cdf5520b1a9
Sam Agnew <sagnew@eden.rutgers.edu>
2014-06-14 11:32:26 -0400

    Change things around a bit

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return render_template('index.html')
-
-@app.route('/test')
-def test():
-	return 'test'
+@app.route('/', methods=['get', 'post', 'put', 'delete'])
+def index():
+    if request.method == 'GET':
+        return "Naw"
+    return "Hello"
 
 if __name__ == '__main__':
-	# if I am being run from the command line.
-	app.run('0.0.0.0', debug = True)
+    # if I am being run from the command line.
+    app.run('0.0.0.0', port=7000, debug = True)
